e54ce74912df03c8672d61ffd6d41b6d8d2165a1
jerkodeur <jerome.potie@gmail.com>
2020-06-25 11:12:00 +0200

    comments the descriptions of the routes and make several adjustment

--- routes/doctors.js
+++ routes/doctors.js
@@ -5,6 +5,7 @@ const connexion = require('../conf')
 
 let today = new Date().toISOString().slice(0, 10)
 
+// Fetch the list of the patient (for the doctor)
 router.get('/:id', (req, res) => {
   let request = 'SELECT p.id, p.firstname, p.lastname FROM patient p '
   request += 'JOIN medication m ON p.id = m.patient_id '
@@ -20,16 +21,16 @@ router.get('/:id', (req, res) => {
     return res.status(200).json(result)
   })
 })
-
+// Fetch The datas of a selected patient to verify if he took his medics
 router.get('/:id/patient/:patient_id', (req, res) => {
   const { id, patient_id } = req.params
-  let request = 'SELECT m.med_name , m.order_name , m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, last_take'
+  let request = 'SELECT m.med_name , m.order_name , m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, m.last_update'
   request += ', p.firstname, p.lastname'
   request += ' FROM medication m'
   request += ' JOIN medication_patient mp ON mp.patient_id = m.patient_id'
   request += ' JOIN patient p ON mp.patient_id = p.id'
   request += ` WHERE m.doctor_id = ? AND p.id = ? AND m.date >= ${today}`
-  request += ' GROUP BY m.med_name, m.order_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, p.firstname, p.lastname'
+  request += ' GROUP BY m.order_name, m.med_name, m.order_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, p.firstname, p.lastname, m.last_update'
   connexion.query(request, [id, patient_id], (err, result) => {
     if (err) {
       return res.status(500).json({
--- routes/patients.js
+++ routes/patients.js
@@ -5,6 +5,7 @@ const connexion = require('../conf')
 
 let today = new Date().toISOString().slice(0, 10)
 
+// Fetch all the medications datas (for the patient)
 router.get('/:id', (req, res) => {
   let request = 'SELECT m.id, m.med_name , m.order_name , m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night'
   request += ', d.firstname, d.lastname, d.specialty'
@@ -12,7 +13,7 @@ router.get('/:id', (req, res) => {
   request += ' JOIN medication_doctor md ON md.doctor_id = m.doctor_id'
   request += ' JOIN doctor d ON md.doctor_id = d.id'
   request += ` WHERE m.patient_id = ? AND m.date >= ${today}`
-  request += ' GROUP BY m.med_name, m.order_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, d.firstname, d.lastname, d.specialty'
+  request += ' GROUP BY m.order_name, m.med_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, m.id, d.firstname, d.lastname, d.specialty'
   connexion.query(request, req.params.id, (err, result) => {
     if (err) {
       return res.status(500).json({
@@ -23,7 +24,7 @@ router.get('/:id', (req, res) => {
     return res.status(200).json(result)
   })
 })
-
+// Get the medication datas filter by date asc (for the patient)
 router.get('/:id/filter/date/', (req, res) => {
   let filterDate = new Date(req.body.filterDate)
   filterDate = filterDate.toLocaleDateString('fr-FR')
@@ -34,7 +35,7 @@ router.get('/:id/filter/date/', (req, res) => {
   request += ' JOIN medication_doctor md ON md.doctor_id = m.doctor_id'
   request += ' JOIN doctor d ON md.doctor_id = d.id'
   request += ` WHERE m.patient_id = ? AND m.date <= CAST('${filterDate}' AS DATE)`
-  request += ' GROUP BY m.med_name, m.order_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, d.firstname, d.lastname, d.specialty'
+  request += ' GROUP BY m.order_name, m.med_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, d.firstname, d.lastname, d.specialty'
   connexion.query(request, req.params.id, (err, result) => {
     if (err) {
       return res.status(500).json({
@@ -46,6 +47,7 @@ router.get('/:id/filter/date/', (req, res) => {
   })
 })
 
+// Get the medication datas filter by doctor (for the patient)
 router.get('/:id/filter/doctor/:doctor_id', (req, res) => {
   const { id, doctor_id } = req.params
   let request = 'SELECT m.med_name , m.order_name , m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night'
@@ -54,7 +56,7 @@ router.get('/:id/filter/doctor/:doctor_id', (req, res) => {
   request += ' JOIN medication_doctor md ON md.doctor_id = m.doctor_id'
   request += ' JOIN doctor d ON md.doctor_id = d.id'
   request += ` WHERE m.patient_id = ? AND m.doctor_id = ?`
-  request += ' GROUP BY m.med_name, m.order_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, d.firstname, d.lastname, d.specialty'
+  request += ' GROUP BY m.order_name, m.med_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, d.firstname, d.lastname, d.specialty'
   connexion.query(request, [id, doctor_id], (err, result) => {
     if (err) {
       return res.status(500).json({
@@ -66,6 +68,7 @@ router.get('/:id/filter/doctor/:doctor_id', (req, res) => {
   })
 })
 
+// Get the medication datas filter by specialty (for the patient)
 router.get('/:id/filter/specialty/:type', (req, res) => {
   const { id, type } = req.params
   let request = 'SELECT m.med_name , m.order_name , m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night'
@@ -74,27 +77,7 @@ router.get('/:id/filter/specialty/:type', (req, res) => {
   request += ' JOIN medication_doctor md ON md.doctor_id = m.doctor_id'
   request += ' JOIN doctor d ON md.doctor_id = d.id'
   request += ` WHERE m.patient_id = ? AND d.specialty = ?`
-  request += ' GROUP BY m.med_name, m.order_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, d.firstname, d.lastname, d.specialty'
-  connexion.query(request, [id, type], (err, result) => {
-    if (err) {
-      return res.status(500).json({
-        message: err.message,
-        sql: err.sql
-      })
-    }
-    return res.status(200).json(result)
-  })
-})
-
-router.get('/:doctor_id/filter/specialty/:type', (req, res) => {
-  const { id, type } = req.params
-  let request = 'SELECT m.med_name , m.order_name , m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night'
-  request += ', d.firstname, d.lastname, d.specialty'
-  request += ' FROM medication m'
-  request += ' JOIN medication_doctor md ON md.doctor_id = m.doctor_id'
-  request += ' JOIN doctor d ON md.doctor_id = d.id'
-  request += ` WHERE m.patient_id = ? AND d.specialty = ?`
-  request += ' GROUP BY m.med_name, m.order_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, d.firstname, d.lastname, d.specialty'
+  request += ' GROUP BY m.order_name, m.med_name, m.date, m.dosage, m.used, m.morning, m.midday, m.evening, m.night, d.firstname, d.lastname, d.specialty'
   connexion.query(request, [id, type], (err, result) => {
     if (err) {
       return res.status(500).json({
